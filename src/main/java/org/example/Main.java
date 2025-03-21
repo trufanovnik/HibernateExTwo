4f8ac40173c5610dfe544da83cf4c2083287019b
unknown <trufanovwork@gmail.com>
2025-03-21 12:18:33 +0300

    test db connection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        try (SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory()) {
+            Session session = sessionFactory.openSession();
+            System.out.println("Подключение к базе данных успешно!");
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
