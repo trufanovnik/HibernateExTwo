05ec43262f3eb8a2b1f3ce61d4b8c68c57acd93b
unknown <trufanovwork@gmail.com>
2025-03-21 15:51:03 +0300

    created Items class

---
+++ src/main/java/org/example/Items.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "items")
+@NoArgsConstructor
+public class Items {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public long id;
+
+    @Column(name = "val")
+    public int val;
+
+    Items(int val){
+        this.val = val;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,6 @@
 package org.example;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-
 public class Main {
     public static void main(String[] args) {
-        try (SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory()) {
-            Session session = sessionFactory.openSession();
-            System.out.println("Подключение к базе данных успешно!");
-            session.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
