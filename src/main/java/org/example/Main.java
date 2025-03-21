18a6d4748740f33c97245f8b3a147b07e3f5cade
unknown <trufanovwork@gmail.com>
2025-03-21 16:48:04 +0300

    create and fill the table

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,25 @@
 package org.example;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
 public class Main {
     public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Items.class)
+                .buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        for (int i = 0; i < 40; i++){
+            session.save(new Items(0));
+        }
+
+        transaction.commit();
+        session.close();
     }
 }
